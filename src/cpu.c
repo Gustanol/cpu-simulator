84b41d5a8ebe4c25a92d72fb912a0b6c444e761f
Gustanol <gualeixos456@gmail.com>
2025-08-25 00:11:34 -0300

    feat: adding some comments

--- src/cpu.c
+++ src/cpu.c
@@ -4,21 +4,37 @@
 #include <string.h>
 
 int run_program(const char *path, const char *mode) {
-    FILE *file;
-    struct cpu c;
-    uint8_t buffer[16];
-    int offset = 0;
+    /*
+     * variables setup
+     */
+    FILE *file;         // file variable
+    struct cpu c;       // defining struct from cpu.h
+    uint8_t buffer[16]; // creating an array to store 16 bytes to form memory in blocks
+    int offset = 0;     // current line of the binary file
+
+    /*
+     * initialization of the variables from the struct
+     */
     c.pc = 0;
     c.addr = 0;
     c.acc = 0;
-    int running = 1;
 
+    int running = 1; // variable to initialize the "infinite" loop
+
+    /*
+     * opens a file using the expected parameters and saves it on file variable
+     */
     file = fopen(path, mode);
     if (file == NULL) {
         perror("Error opening file");
         return -1;
     }
 
+    /*
+     * read 16 bytes at a time and saves into buffer array
+     *
+     * in each iteration, copy buffer into c.memory array
+     */
     int read = fread(buffer, 1, 16, file);
     while (read > 0) {
         memcpy(c.memory + offset, buffer, read);
@@ -26,35 +42,33 @@ int run_program(const char *path, const char *mode) {
         read = fread(buffer, 1, 16, file);
     }
 
-    /*
-    for (int i = 0; i < 256; i++) {
-        printf("%x ", c.memory[i]);
-    }
-    printf("\n");
-*/
     while (running) {
-        // printf("%d: %x ", c.pc, c.memory[c.pc]);
+        /*
+         * opcode refers to an instruction
+         *
+         * it is followed by an operand
+         */
         int16_t opcode = c.memory[c.pc++];
         switch (opcode) {
-        case 0x01:
+        case 0x01: // LOAD
             c.addr = c.memory[c.pc++];
             c.acc = c.memory[c.addr];
             break;
-        case 0x02:
+        case 0x02: // LOADI
             c.acc = c.memory[c.pc++];
             break;
-        case 0x03:
+        case 0x03: // STORE
             c.addr = c.memory[c.pc++];
             c.memory[c.addr] = c.acc;
             break;
-        case 0x04:
+        case 0x04: // OUT
             c.addr = c.memory[c.pc++];
             printf("%c", c.memory[c.addr]);
             break;
-        case 0xFF:
+        case 0xFF: // HILT
             running = 0;
             break;
-        case 0x20:
+        case 0x20: // SPACE (in ASCII)
             printf(" ");
             break;
         default:
--- src/main.c
+++ src/main.c
@@ -1,6 +1,9 @@
 #include "cpu.h"
 
 int main(int argc, char *argv[]) {
+    /*
+     * calls the function and pass the parameters
+     */
     run_program("/home/gustanol/projects/cpu-simulator/tests/program.bin", "rb");
     return 0;
 }
